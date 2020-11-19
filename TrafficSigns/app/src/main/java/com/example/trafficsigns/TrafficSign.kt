ace69fdc08ef082feb2d490fca36a7ddb49881ff
Botond Halasz <halasz_boti_99@yahoo.com>
2020-11-19 15:54:22 +0200

    Get data from dropbox

---
+++ TrafficSigns/app/src/main/java/com/example/trafficsigns/TrafficSign.kt
@@ -0,0 +1,9 @@
+package com.example.trafficsigns
+
+data class TrafficSign(
+        var name: String? = null,
+        var image: String? = null,
+        var group: String? = null,
+        var traffic_rules: String? = null,
+        var description: String? = null
+)
