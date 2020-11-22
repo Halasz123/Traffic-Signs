e132dcecccd675b099cc39afd9c70f48fe90631f
Botond Halasz <halasz_boti_99@yahoo.com>
2020-11-22 12:26:40 +0200

    no message

--- TrafficSigns/app/src/main/java/com/example/trafficsigns/ui/fragments/List/TrafficCollectionListAdapter.kt
+++ TrafficSigns/app/src/main/java/com/example/trafficsigns/ui/fragments/List/TrafficCollectionListAdapter.kt
@@ -5,7 +5,7 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class TrafficCollectionListAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
-    override fun getItemCount(): Int = 100
+    override fun getItemCount(): Int = 5
 
     override fun createFragment(position: Int): Fragment {
         // Return a NEW fragment instance in createFragment(int)
