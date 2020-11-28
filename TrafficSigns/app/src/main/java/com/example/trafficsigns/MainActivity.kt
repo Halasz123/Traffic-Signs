efcde6db4587219ed8525635da3864736964090a
Botond Halasz <halasz_boti_99@yahoo.com>
2020-11-28 21:48:36 +0200

    navigation_drawer added to home screen and bank profile fragment

--- TrafficSigns/app/src/main/java/com/example/trafficsigns/MainActivity.kt
+++ TrafficSigns/app/src/main/java/com/example/trafficsigns/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.trafficsigns
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.trafficsigns.databinding.ActivityMainBinding
 import com.example.trafficsigns.ui.fragments.Home.MainScreenFragment
 
@@ -14,7 +17,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        //supportFragmentManager.beginTransaction().replace(R.id.main_framelayout, MainScreenFragment()).commit()
     }
-
 }
